e11f7a5a2123be84c028758cfea6a38407ab433a
Nickey Gertsberg <ngertsberg@diginsights.com>
2020-02-18 16:51:05 -0500

    add a loader

--- src/Components/MarsRover/components/Loading/index.js
+++ src/Components/MarsRover/components/Loading/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './index.sass';
+import './index.scss';
 const Loading = () => {
     return(
-        <div id = 'Loading'>
-            <p>Loading...</p>
-        </div>
+            <div id="loader-wrapper">
+                <div id="loader"></div>
+
+
+            </div>
     )
 }
 
